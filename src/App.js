e3a71ec9817a684d53caaac157d5912ee57c11a3
Asif Mustafayev <53805470+Asifdvp@users.noreply.github.com>
2022-06-19 17:17:14 +0400

    state

--- src/App.js
+++ src/App.js
@@ -2,28 +2,29 @@ import "./App.css";
 import Navi from "./Navi";
 import ProductList from "./ProductList";
 import CategoryList from "./CategoryList";
-import { Container,Row,Col } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 // import Container from 'react-bootstrap/Container';
 // import Row from 'react-bootstrap/Row';
 // import Col from 'react-bootstrap/Col';
 function App() {
- let  categoryInfo = {title:"Category List"};
- let  productInfo = {title: "Product List"};
+  let categoryInfo = { title: "Category List" };
+  let productInfo =  { title: "Product List" };
   return (
     <div>
       <Container>
         <Row>
-          <Navi/>
+          <Navi />
         </Row>
-<Row> 
-<Col xs="3"> <CategoryList  info={categoryInfo} /> </Col>
-<Col xs="9">  <ProductList info={productInfo}  />  </Col>
-  
-  
-  </Row>
-      
-         
-        
+        <Row>
+          <Col xs="3">
+            {" "}
+            <CategoryList info={categoryInfo} />{" "}
+          </Col>
+          <Col xs="9">
+            {" "}
+            <ProductList info={productInfo} />{" "}
+          </Col>
+        </Row>
       </Container>
     </div>
   );
--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,41 +1,31 @@
 import React, { Component } from 'react'
 import { ListGroup,ListGroupItem} from 'reactstrap'
 export default class CategoryList extends Component {
+  state={
+    categories : [
+      {categoryId : 1 , categoryName :"Beverages"},
+      {categoryId : 2 , categoryName :"Condiments"}
+    ]
+  }
   render() {
     return (
       <div>
         <h3>{this.props.info.title} </h3>
 <ListGroup>
-  <ListGroupItem 
-  action
-      href="#"
-      tag="a">
-    Cras justo odio
-  </ListGroupItem>
-  <ListGroupItem 
-  action
-      href="#"
-      tag="a">
-    Cras justo odio
-  </ListGroupItem>
-  <ListGroupItem 
-  action
-      href="#"
-      tag="a">
-    Cras justo odio
-  </ListGroupItem>
-  <ListGroupItem 
-  action
-      href="#"
-      tag="a">
-    Cras justo odio
-  </ListGroupItem>
-  <ListGroupItem 
-  action
-      href="#"
-      tag="a">
-    Cras justo odio
-  </ListGroupItem>
+{
+  this.state.categories.map(item =>(
+    <ListGroupItem 
+    action
+        href="#"
+        tag="a">
+      {item.categoryName}
+    </ListGroupItem>
+  ))
+}
+
+
+
+
 
 </ListGroup>
         </div>
